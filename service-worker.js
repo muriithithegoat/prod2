073c32201ffd072d3109e224c76853ceeb2fc635
DevMuriithi <kinyuaangelo@gmail.com>
2025-08-12 16:01:33 +0300

    up up

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = "daily-minimal-v1";
+const ASSETS = ["index.html","manifest.json","icon.png","/"];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
+});
